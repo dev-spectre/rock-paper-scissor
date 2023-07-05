1c0155581e542240d03c4a38a36647ed5a7dec85
dev-spectre <abhishekdallasalpy@gmail.com>
2023-07-05 21:30:06 +0530

    Update script.js Completed script.js and used aysnc/await instead of setTimeouts to avoid multiple nested setTimeouts.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 let score = {
     "player": 0,
-    "computer": 4,
+    "computer": 0,
 };
+
+let isRoundPlaying = false;
 
 const options = document.querySelectorAll("#options > div[id]");
 options.forEach(option => {
 
-    option.addEventListener("click", () => { 
+    option.addEventListener("click", () => {
+        if (isRoundPlaying) return;
+        isRoundPlaying = true;
+
         const matchResults = document.querySelectorAll(".match-result");
+        const playerScore = 
+            document.querySelector("#player-score .gradient-border > div");
+        const computerScore = 
+            document.querySelector("#computer-score .gradient-border > div");
 
         if (score["player"] >= 5 || score["computer"] >= 5) {
             matchResults.forEach(matchResult => 
                 matchResult.setAttribute("data-match-result", "none"));
             score["player"] = 0;
             score["computer"] = 0;
+            playerScore.innerHTML = "";
+            computerScore.innerHTML = "";
         }
 
         const playerSelection = option.id;
@@ -24,108 +35,88 @@ options.forEach(option => {
         if (roundWinner) score[roundWinner]++;
         
         updateGui(playerSelection, computerSelection, score);
-
         
-        setTimeout(() => {
-            if (score["player"] === 5) {
-                matchResults.forEach(matchResult => {
-                    matchResult.setAttribute("data-match-result","won");
-                    const resultText = document.querySelector(".match-result div");
-                    resultText.innerText = "YOU WON"
-                });
-            } else if (score["computer"] === 5) {
-                matchResults.forEach(matchResult => {
-                    matchResult.setAttribute("data-match-result", "lost");
-                    const resultText = document.querySelector(".match-result div");
-                    resultText.innerText = "YOU LOST"
-                });
-            }
-            matchResults.addEventListener("change", () => {
-                if (score["player"] === 5) {
-                    matchResults.forEach(() => {
-                        const resultText = document.querySelector(".match-result div");
-                        resultText.innerText = "YOU WON"
-                    });
-                } else if (score["computer"] === 5) {
-                    matchResults.forEach(() => {
-                        const resultText = document.querySelector(".match-result div");
-                        resultText.innerText = "YOU LOST"
-                    });
-                }
-            })
-        }, 4700);
     });
-
 
 });
 
-function updateGui(playerSelection, computerSelection, score) {
-    const timeout = 100;
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+function updateGui(playerSelection, computerSelection, score) {
     const playerHexQuery = "#player-hex .inner-hex + img";
     const computerHexQuery = "#computer-hex .inner-hex + img";
 
+    const playerHex = document.querySelector("#player-hex .outer-hex");
+    const computerHex = document.querySelector("#computer-hex .outer-hex");
+    const matchResults = document.querySelectorAll(".match-result div");
+
     const playerSelectionImg = document.querySelector(playerHexQuery);
+    const computerSelectionImg = document.querySelector(computerHexQuery);
+
     playerSelectionImg.classList.remove("scale-zero");
     playerSelectionImg.src = `./assets/${playerSelection}.svg`;
     
-    playerSelectionImg.addEventListener("transitionend", () => {       
-        const playerHex = document.querySelector("#player-hex .outer-hex");
-        const computerHex = document.querySelector("#computer-hex .outer-hex");
-        const matchResults = document.querySelectorAll(".match-result div");
-        setTimeout(() => {
-            
-            matchResults.forEach(matchResult => matchResult.innerText = "...");
-        
-            playerHex.classList.add("hidden");
-            computerHex.classList.remove("hidden");
-
-        }, timeout);
-
-        setTimeout(() => {
-            const computerSelectionImg = document.querySelector(computerHexQuery);
+    playerSelectionImg.addEventListener("transitionend", async () => {       
+        //* computer's turn
+        matchResults.forEach(matchResult => matchResult.innerText = "...");
     
-            computerSelectionImg.classList.remove("scale-zero");
-            computerSelectionImg.src = `./assets/${computerSelection}.svg`;
-        }, timeout + 500);
+        playerHex.classList.add("hidden");
+        computerHex.classList.remove("hidden");
 
-        const playerSelectionImg = document.querySelector(playerHexQuery);
-        const computerSelectionImg = document.querySelector(computerHexQuery);
+        //* display computer's choice
+        await delay(300);
+        computerSelectionImg.classList.remove("scale-zero");
+        computerSelectionImg.src = `./assets/${computerSelection}.svg`;
+    }, { once: true });
         
-        setTimeout(() => {
-            const playerScore = 
-                document.querySelector("#player-score .gradient-border > div");
-            const computerScore = 
-                document.querySelector("#computer-score .gradient-border > div");
-    
-            const point = "<div class=point></div>"
-            let totalPoints = ""
-            for (let i=0; i<score["player"]; i++) {
-                totalPoints += point;
+
+    computerSelectionImg.addEventListener("transitionend", async () => {
+        await delay(800);
+
+        //* add points
+        const playerScore = 
+            document.querySelector("#player-score .gradient-border > div");
+        const computerScore = 
+            document.querySelector("#computer-score .gradient-border > div");
+
+        const point = "<div class=point></div>";
+        let totalPoints = ""
+        for (let i=0; i<score["player"]; i++) {
+            totalPoints += point;
+        }
+        playerScore.innerHTML = totalPoints;
+
+        totalPoints = "";
+        for (let i=0; i<score["computer"]; i++) {
+            totalPoints += point;
+        }
+        computerScore.innerHTML = totalPoints;
+
+        await delay(400);
+        //* shrink images
+        playerSelectionImg.classList.add("scale-zero");
+        computerSelectionImg.classList.add("scale-zero");
+
+        //* player's turn
+        matchResults.forEach(matchResult => matchResult.innerText = "YOUR TURN");
+        
+        computerHex.classList.add("hidden");
+        playerHex.classList.remove("hidden");
+
+        //* at match end
+        matchResults.forEach(matchResult => {
+            if (score["player"] === 5) {
+                matchResult.innerText = "YOU WON";
+                matchResult.parentElement.setAttribute("data-match-result","won");
+            } else if (score["computer"] === 5) {
+                matchResult.innerText = "YOU LOST";
+                matchResult.parentElement.setAttribute("data-match-result", "lost");
             }
-            playerScore.innerHTML = totalPoints;
+        });
 
-            totalPoints = "";
-            for (let i=0; i<score["computer"]; i++) {
-                totalPoints += point;
-            }
-            computerScore.innerHTML = totalPoints;
-            
-        }, timeout + 800);
-    
-        setTimeout(() => {
-            //* shrink
-            playerSelectionImg.classList.add("scale-zero");
-            computerSelectionImg.classList.add("scale-zero");
-    
-        }, timeout + 1800);
-        
-        setTimeout(() => {
-            matchResults.forEach(matchResult => matchResult.innerText = "YOUR TURN");
-            
-            computerHex.classList.add("hidden");
-            playerHex.classList.remove("hidden");
-        }, timeout + 2100)
+         isRoundPlaying = false;
     }, { once: true });
 }
 
@@ -151,32 +142,5 @@ function playRound(playerSelection, computerSelection) {
 
     } else {
         return "computer";
-    }
-}
-
-function game() {
-
-    while (score["player"] !== 5 && score["computer"] !== 5) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
-
-        if (playerSelection === null) {
-            return;
-        }
-
-        const roundWinner = playRound(playerSelection, computerSelection);
-        if (roundWinner) score[roundWinner]++;
-
-        console.log("Score");
-        console.table(score);
-    }
-
-    if (score["player"] === 5) {
-        console.log("%c YOU WON!!!",
-        "color: green; font-size: 25px;");
-
-    } else {
-        console.log("%c YOU LOSE!!",
-        "color: red; font-size: 25px;");
     }
 }
